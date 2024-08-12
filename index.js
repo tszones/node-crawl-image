e1e5cb936ef6dd04c58179441c547a9afbabf7d4
shizheng <64840669+tszones@users.noreply.github.com>
2024-08-12 16:20:18 +0800

    add comments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 // 图片 URL 列表
+// List of image URLs
 const imageUrls = [
   "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
   "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
@@ -19,6 +20,7 @@ const imageUrls = [
 ];
 
 // 下载图片的函数
+// Function to download images
 const downloadImage = async (url, filepath) => {
   const writer = fs.createWriteStream(filepath);
 
@@ -37,12 +39,14 @@ const downloadImage = async (url, filepath) => {
 };
 
 // 创建存储图片的文件夹
+// Create directory to store images
 const downloadDir = path.resolve(__dirname, 'downloaded_images');
 if (!fs.existsSync(downloadDir)) {
   fs.mkdirSync(downloadDir);
 }
 
 // 下载所有图片
+// Download all images
 const downloadAllImages = async () => {
   for (const url of imageUrls) {
     const filename = path.basename(url);
